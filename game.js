af15b004323da385ae63dc2176a594d44a57c289
sw-yx <shawnthe1@gmail.com>
2017-07-27 18:35:37 -0400

    done

--- game.js
+++ game.js
@@ -24,6 +24,27 @@ var gameOfLife = {
     var board = document.getElementById('board');
     board.appendChild(goltable);
     
+    //setup Button events
+    var clear_btn = document.getElementById('clear_btn');
+    clear_btn.addEventListener('click', e => {
+      this.forEachCell(function (cell) {
+        cell.className = 'dead';
+        cell.dataset.status = 'dead';
+      })
+    })
+    var reset_btn = document.getElementById('reset_btn');
+    reset_btn.addEventListener('click', e => {
+      this.forEachCell(function (cell) {
+        var status = Math.round(Math.random())
+        cell.className = status ? 'dead' : 'alive';
+        cell.dataset.status = status ? 'dead' : 'alive';
+      })
+    })
+    var step_btn = document.getElementById('step_btn');
+    step_btn.addEventListener('click', this.step.bind(this));
+    var play_btn = document.getElementById('play_btn');
+    play_btn.addEventListener('click', this.enableAutoPlay.bind(this));
+
     // once html elements are added to the page, attach events to them
     this.setupBoardEvents();
   },
@@ -35,6 +56,9 @@ var gameOfLife = {
       and pass into func, the cell and the cell's x & y
       coordinates. For example: iteratorFunc(cell, x, y)
     */
+    var cell00 = document.querySelectorAll('td');
+    // cell00.forEach(iteratorFunc.bind(document.body));
+    cell00.forEach((cell, i) => iteratorFunc(cell, i));
   },
   
   setupBoardEvents: function() {
@@ -53,7 +77,7 @@ var gameOfLife = {
     // You need to add the click event on EVERY cell on the board
     
     var onCellClick = function (e) {
-      
+      // console.log(this)
       // QUESTION TO ASK YOURSELF: What is "this" equal to here?
       
       // how to set the style of the cell when it's clicked
@@ -67,9 +91,48 @@ var gameOfLife = {
       
     };
     
-    var cell00 = document.getElementById('0-0');
-    cell00.addEventListener('click', onCellClick);
+    this.forEachCell(x => x.addEventListener('click', onCellClick));
   },
+
+  getNeighbors: function(i) {
+    //toprow
+    if (i < 12) {
+      var toprow = []
+    } else {
+
+      if (i % 12 == 0) {
+        var toprow = [i-12, i-11]
+      } else if (i % 12 == 11) {
+        var toprow = [i-13, i-12]
+      } else {
+        var toprow = [i-13, i-12, i-11]
+      }
+    }
+    //midrow
+    if (i % 12 == 0) {
+      var midrow = [i + 1]
+    } else if (i % 12 == 11) {
+      var midrow = [i - 1]
+    } else {
+      var midrow = [i-1, i+1]
+    }
+    //botrow
+    if (i > (143 - 12)) {
+      var botrow = []
+    } else {
+
+      if (i % 12 == 0) {
+        var botrow = [i+13, i+12]
+      } else if (i % 12 == 11) {
+        var botrow = [i+12, i+11]
+      } else {
+        var botrow = [i+13, i+12, i+11]
+      }
+      
+    }
+    return toprow.concat(midrow.concat(botrow))
+  },
+
 
   step: function () {
     // Here is where you want to loop through all the cells
@@ -80,11 +143,42 @@ var gameOfLife = {
     // You need to:
     // 1. Count alive neighbors for all cells
     // 2. Set the next state of all cells based on their alive neighbors
+    
+    var oldState = []
+    this.forEachCell((cell) => {oldState.push(cell.dataset.status)})
+    var newState = []
+
+    oldState.forEach((v, i) => {
+        var neighbors = this.getNeighbors(i)
+          neighbors = neighbors.filter(x => oldState[x]==='alive')
+          switch (neighbors.length){
+            case 2:
+            case 3:
+              newState.push('alive')
+              break;
+            default:
+              newState.push('dead')
+          }
+          this.forEachCell((cell, i) => {
+            var status = newState[i]
+            cell.className = status;
+            cell.dataset.status = status;
+          })
+    })
+      
   },
 
   enableAutoPlay: function () {
     // Start Auto-Play by running the 'step' function
     // automatically repeatedly every fixed time interval  
+    if (!this.stepInterval) {
+      // kick it off
+      this.stepInterval = setInterval(this.step.bind(this), Number(prompt("how fast do you want it", 200)))
+    } else {
+      // kill it
+      clearInterval(this.stepInterval);
+      this.stepInterval = null
+    }
   }
   
 };
